de96725f02fa44c8b10cfbb0bf35ee7ce7b82cc2
GuoHai <yiim@foxmail.com>
2021-04-25 17:17:09 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     platforms=["any"],
     classifiers=filter(None, classifiers.split("\n")),
     install_requires=[
-        "pymongo >= 2.9",
+        "pymongo @ git+https://github.com/seamys/mongo-python-driver.git@b3.11.1",
         "importlib_metadata>=0.6",
         "autocommand",
         "importlib_resources",
